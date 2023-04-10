8a23146c8d0882e2af6be2edbf6b99ca48ea49b5
Krzysztof Jankowski <kj@p1x.in>
2023-04-10 11:10:38 +0200

    p1x logo 70%

--- shader.glsl
+++ shader.glsl
@@ -1,5 +1,5 @@
 /*
- * SHADER BY KRZYSZTOF KRYSTIAN JANKOWSKK
+ * SHADER BY KRZYSZTOF KRYSTIAN JANKOWSKI
  * (c)2023.04 P1X
  *
  * */
@@ -9,7 +9,10 @@ varying vec2 v_uv;
 uniform float u_time;
 uniform vec2 u_resolution;
 
-// HELPERS
+/*
+ * SDF BRUSHES
+ *
+ * */
 float sdSphere( vec3 p, float s ){return length(p)-s;}
 
 float sdBox( vec3 p, vec3 b )
@@ -24,6 +27,10 @@ float sdRoundBox( vec3 p, vec3 b, float r )
   return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0) - r;
 }
 
+/*
+ * SDF OPs
+ *
+ * */
 float opUnion( float d1, float d2 ) { return min(d1,d2); }
 
 float opSubtraction( float d1, float d2 ) { return max(-d1,d2); }
@@ -42,25 +49,40 @@ float opSmoothIntersection( float d1, float d2, float k ) {
     float h = clamp( 0.5 - 0.5*(d2-d1)/k, 0.0, 1.0 );
     return mix( d2, d1, h ) + k*h*(1.0-h); }
 
-// MAP WORLD SDFs
+/*
+ * COMBINE SDF WORLD
+ *
+ * */
 float map(in vec3 pos){
-    float sphere0 = sdSphere(pos, 0.2);
-    float plane = pos.y + 0.2;
-    float box0 = sdRoundBox(
-        pos+vec3(0.25,sin(u_time)*0.2,cos(u_time)*0.3),
-        vec3(0.1),
-        0.02);
-    float box1 = sdRoundBox(
-        pos+vec3(sin(u_time)*0.25,-0.1,cos(u_time)*0.1),
-        vec3(0.1),
-        0.02);
+    float ground = pos.y + 0.1;
 
-    float scene=opSmoothUnion(sphere0, opUnion(box0,box1), 0.1);
+    pos -= vec3(.0,-0.8+sin(-1.5+u_time*.5)*1.4,.0);
 
-    return opSmoothUnion(plane,scene,0.1);
+
+    float p1 = sdBox(pos-vec3(-0.7,0.95,.0),vec3(0.05,0.95,0.05));
+    float p2 = sdBox(pos-vec3(-0.5,1.85,.0),vec3(0.15,0.05,0.05));
+    float p3 = sdBox(pos-vec3(-0.5,0.95,.0),vec3(0.15,0.05,0.05));
+    float p4 = sdBox(pos-vec3(-0.3,1.40,.0),vec3(0.05,0.50,0.05));
+    float p_ = opUnion(p1,opUnion(p4,opUnion(p2,p3)));
+
+    float i1 = sdBox(pos-vec3(.0,0.95,.0),vec3(0.05,0.95,0.05));
+    float i2 = sdBox(pos-vec3(-.1,1.75,.0),vec3(0.05,0.05,0.05));
+    float i_ = opUnion(i1,i2);
+
+    float x1 = sdBox(pos-vec3(0.3,1.55,.0),vec3(0.05,0.35,0.05));
+    float x2 = sdBox(pos-vec3(0.7,1.55,.0),vec3(0.05,0.35,0.05));
+    float x3 = sdBox(pos-vec3(0.3,0.45,.0),vec3(0.05,0.45,0.05));
+    float x4 = sdBox(pos-vec3(0.7,0.45,.0),vec3(0.05,0.45,0.05));
+    float x_ = opUnion(x1,opUnion(x2,opUnion(x3,x4)));
+
+    float scene=opUnion(p_,opUnion(i_,x_));
+    return opSmoothUnion(ground,scene,0.1);
 }
 
-// NORMALS
+/*
+ * NORMALS
+ *
+ * */
 vec3 calcNormal( in vec3 pos){
     vec2 e = vec2(0.0001,0.0);
     return normalize(
@@ -69,7 +91,10 @@ vec3 calcNormal( in vec3 pos){
              map(pos+e.yyx)-map(pos-e.yyx)));
 }
 
-// RAYCASTING
+/*
+ * RAYCASTING
+ *
+ * */
 float castRay(in vec3 ro, vec3 rd){
    float t = 0.0;
     for (int i=0; i<100; i++){
@@ -83,10 +108,13 @@ float castRay(in vec3 ro, vec3 rd){
     return t;
 }
 
-// MAIN RENDERER
+/*
+ * THE RENDERER
+ *
+ * */
 vec3 render(in vec2 p){
-    vec3 ro = vec3(.0,.0,0.5);
-    vec3 ta = vec3(.0,.0,.0);
+    vec3 ro = vec3(.0,.0,1.5-sin(u_time*.5)*.5);
+    vec3 ta = vec3(.0,.5+sin(u_time*.5)*.3,.0);
 
     vec3 ww = normalize (ta-ro);
     vec3 uu = normalize( cross(ww, vec3(0,1,0)));
@@ -118,12 +146,18 @@ vec3 render(in vec2 p){
     return col;
 }
 
-// UV SCREEN RATIO
+/*
+ * UV SCREEN RATIO
+ *
+ * */
 vec2 getUV(in vec2 fragCoord, vec2 offset){
   return (2.0*fragCoord * u_resolution.xy)/ u_resolution.y;
 }
 
-// 4xAA
+/*
+ * 4xAA
+ *
+ * */
 vec3 renderAAAA(in vec2 fragCoord){
     vec4 e =vec4(0.125,-0.125,0.375,-0.375);
     vec3 colAA = render(getUV(fragCoord,e.xz))+
@@ -134,12 +168,19 @@ vec3 renderAAAA(in vec2 fragCoord){
     return colAA/=4.0;
 }
 
+/*
+ * MAIN
+ *
+ * */
 void main() {
-  vec3 col = renderAAAA(v_uv);
-  //vec3 col = render(getUV(v_uv,vec2(.0,.0)));
+  //vec3 col = renderAAAA(v_uv);
+  vec3 col = render(getUV(v_uv,vec2(.0,.0)));
 
   col = pow (col, vec3(0.4545));
   gl_FragColor = vec4(col, 1.0);
 }
 
-// END
+/*
+ * END OF LISTING
+ *
+ * */
